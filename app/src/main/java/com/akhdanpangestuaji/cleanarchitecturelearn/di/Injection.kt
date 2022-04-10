b69a9b80519e79fe807f2ba93b82fe840929dc0d
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-04-10 11:10:34 +0700

    create provide repository fun

--- app/src/main/java/com/akhdanpangestuaji/cleanarchitecturelearn/di/Injection.kt
+++ app/src/main/java/com/akhdanpangestuaji/cleanarchitecturelearn/di/Injection.kt
@@ -1,10 +1,20 @@
 package com.akhdanpangestuaji.cleanarchitecturelearn.di
 
+import com.akhdanpangestuaji.cleanarchitecturelearn.data.MessageRepository
+import com.akhdanpangestuaji.cleanarchitecturelearn.domain.IMessageRepository
+import com.akhdanpangestuaji.cleanarchitecturelearn.domain.MessageInteractor
 import com.akhdanpangestuaji.cleanarchitecturelearn.domain.MessageUseCase
 
 object Injection {
     fun provideUseCase(): MessageUseCase {
         val messageRepository = provideRepository()
-        return  messageRepository(messageRepository)
+        return  MessageInteractor(messageRepository)
     }
+
+    private fun provideRepository(): IMessageRepository{
+        val messageDataSource = provideDataSource()
+        return MessageRepository(messageDataSource)
+    }
+
+
 }
